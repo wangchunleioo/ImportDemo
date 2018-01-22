e0164fdf1159909e0532b13216b21fcb2b97dfef
wangchuanlei <leizi521yy@163.com>
2018-01-22 16:13:42 +0800

    增加商业金额计算方法 BigDecimal

--- ImportDemo/app/src/main/java/com/importdemo/MainActivity.java
+++ ImportDemo/app/src/main/java/com/importdemo/MainActivity.java
@@ -1,13 +1,80 @@
 package com.importdemo;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.importdemolibrary.Calculate;
+import com.importdemolibrary.L;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.et1)
+    EditText mEt1;
+    @Bind(R.id.et2)
+    EditText mEt2;
+    @Bind(R.id.btn_add)
+    Button mBtnAdd;
+    @Bind(R.id.btn_sub)
+    Button mBtnSub;
+    @Bind(R.id.btn_mul)
+    Button mBtnMul;
+    @Bind(R.id.btn_divide)
+    Button mBtnDivide;
+    @Bind(R.id.tv)
+    TextView mTv;
+    @Bind(R.id.activity_main)
+    RelativeLayout mActivityMain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        setListener();
+    }
+
+    private void setListener() {
+        mBtnAdd.setOnClickListener(this);
+        mBtnSub.setOnClickListener(this);
+        mBtnMul.setOnClickListener(this);
+        mBtnDivide.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (!mEt1.getText().toString().isEmpty() && !mEt2.getText().toString().isEmpty()) {
+            switch (view.getId()) {
+                case R.id.btn_add:
+                    mTv.setText("" + Calculate.add(mEt1.getText().toString(), mEt2.getText().toString()));
+                    break;
+                case R.id.btn_sub:
+                    mTv.setText("" + Calculate.sub(mEt1.getText().toString(), mEt2.getText().toString()));
+                    break;
+                case R.id.btn_mul:
+                    try {
+                        mTv.setText("" + Calculate.mul(mEt1.getText().toString(), mEt2.getText().toString(),2));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+                case R.id.btn_divide:
+                    try {
+                        mTv.setText("" + Calculate.divide(mEt1.getText().toString(), mEt2.getText().toString(), 2));
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+            }
+        } else {
+            L.t(MainActivity.this, "请输入正确的数字");
+        }
+
     }
 }
--- ImportDemo/importdemolibrary/src/main/java/com/importdemolibrary/Calculate.java
+++ ImportDemo/importdemolibrary/src/main/java/com/importdemolibrary/Calculate.java
@@ -1,13 +1,71 @@
 package com.importdemolibrary;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 /**
  * Created by wang on 2018/1/20 0020.
  */
 
 public class Calculate {
-    //加法
-    public static int addition(int a, int b) {
-        System.out.println("" + a + b);
-        return a + b;
+    /**
+     * 加法
+     *
+     * @param value1 被加数
+     * @param value2 加数
+     * @return 两数之和
+     */
+    public static Double add(String value1, String value2) {
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.add(b2).doubleValue();
+    }
+
+    /**
+     * 减法
+     *
+     * @param value1 被减数
+     * @param value2 减数
+     * @return 两数之差
+     */
+    public static Double sub(String value1, String value2) {
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.subtract(b2).doubleValue();
+    }
+
+    /**
+     * 乘法
+     *
+     * @param value1 被乘数
+     * @param value2 乘数
+     * @param scale  精确小数点几位
+     * @return 两数之积
+     */
+    public static Double mul(String value1, String value2, Integer scale) throws IllegalAccessException {
+        if (scale < 0) {
+            throw new IllegalAccessException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.multiply(b2).setScale(scale, RoundingMode.HALF_UP).doubleValue();
+    }
+
+    /**
+     * 除法
+     *
+     * @param value1 被除数
+     * @param value2 除数
+     * @param scale  精确小数点几位
+     * @return 两数之商
+     * @throws IllegalAccessException
+     */
+    public static Double divide(String value1, String value2, Integer scale) throws IllegalAccessException {
+        if (scale < 0) {
+            throw new IllegalAccessException("The scale must be a positive integer or zero");
+        }
+        BigDecimal b1 = new BigDecimal(value1);
+        BigDecimal b2 = new BigDecimal(value2);
+        return b1.divide(b2, scale, RoundingMode.HALF_UP).doubleValue();
     }
 }
